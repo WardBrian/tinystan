64b54c709f1614d26716ee55852b1ed3cfe3f0c5
Brian Ward <bward@flatironinstitute.org>
2024-03-21 12:45:57 -0400

    Fix some tests

--- clients/julia/test/runtests.jl
+++ clients/julia/test/runtests.jl
@@ -6,18 +6,17 @@ const STAN_FOLDER = joinpath(@__DIR__, "..", "..", "..", "test_models")
 
 
 
+gaussian_model = Model(joinpath(STAN_FOLDER, "gaussian", "gaussian_model.so"))
+empty_model = Model(joinpath(STAN_FOLDER, "empty", "empty_model.so"))
+multimodal_model = Model(joinpath(STAN_FOLDER, "multimodal", "multimodal_model.so"))
+simple_jacobian_model =
+    Model(joinpath(STAN_FOLDER, "simple_jacobian", "simple_jacobian_model.so"))
+bernoulli_model = Model(joinpath(STAN_FOLDER, "bernoulli", "bernoulli_model.so"))
+BERNOULLI_DATA = "{\"N\": 10, \"y\": [0,1,0,0,0,0,0,0,0,1]}"
+
 
 @testset "FFIStan tests" verbose = true begin
     include("test_compile.jl")
-
-    gaussian_model = Model(joinpath(STAN_FOLDER, "gaussian", "gaussian_model.so"))
-    empty_model = Model(joinpath(STAN_FOLDER, "empty", "empty_model.so"))
-    multimodal_model = Model(joinpath(STAN_FOLDER, "multimodal", "multimodal_model.so"))
-    simple_jacobian_model =
-        Model(joinpath(STAN_FOLDER, "simple_jacobian", "simple_jacobian_model.so"))
-    bernoulli_model = Model(joinpath(STAN_FOLDER, "bernoulli", "bernoulli_model.so"))
-    BERNOULLI_DATA = "{\"N\": 10, \"y\": [0,1,0,0,0,0,0,0,0,1]}"
-
     include("test_model.jl")
     include("test_sample.jl")
     include("test_pathfinder.jl")
