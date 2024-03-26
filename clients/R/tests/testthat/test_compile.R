4f50b2c86c79ff71b274760a75dcfe7795607bdb
Brian Ward <bward@flatironinstitute.org>
2024-03-26 12:50:01 -0400

    R: re-enable other tests

--- clients/R/tests/testthat/test_compile.R
+++ clients/R/tests/testthat/test_compile.R
@@ -1,31 +1,35 @@
+test_that("compilation works", {
+    name <- "gaussian"
+    file <- file.path(stan_folder, name, paste0(name, ".stan"))
 
+    lib <- file.path(stan_folder, name, paste0(name, "_model.so"))
+    unlink(lib, force = TRUE)
 
-# test_that('compilation works', { name <- 'gaussian' file <-
-# file.path(stan_folder, name, paste0(name, '.stan'))
+    out <- compile_model(file, stanc_args = c("--O1"))
 
-# lib <- file.path(stan_folder, name, paste0(name, '_model.so')) unlink(lib,
-# force = TRUE)
+    expect_true(file.exists(lib))
+    expect_equal(normalizePath(lib), normalizePath(out))
 
-# out <- compile_model(file, stanc_args = c('--O1'))
+    unlink(lib, force = TRUE)
 
-# expect_true(file.exists(lib)) expect_equal(normalizePath(lib),
-# normalizePath(out))
+    out <- compile_model(file)
+})
 
-# unlink(lib, force = TRUE)
+test_that("compilation fails on non-stan file", {
+    expect_error(compile_model(file.path(stan_folder, "bernoulli", "bernoulli.data.json")),
+        "does not end with '.stan'")
+})
 
-# out <- compile_model(file) })
+test_that("compilation fails on missing file", {
+    expect_error(compile_model("badpath.stan"), "does not exist!")
+})
 
-# test_that('compilation fails on non-stan file', {
-# expect_error(compile_model(file.path(stan_folder, 'bernoulli',
-# 'bernoulli.data.json')), 'does not end with '.stan'') })
+test_that("compilation fails on bad syntax", {
+    expect_error(compile_model(file.path(stan_folder, "syntax_error", "syntax_error.stan")),
+        "Compilation failed")
+})
 
-# test_that('compilation fails on missing file', {
-# expect_error(compile_model('badpath.stan'), 'does not exist!') })
-
-# test_that('compilation fails on bad syntax', {
-# expect_error(compile_model(file.path(stan_folder, 'syntax_error',
-# 'syntax_error.stan')), 'Compilation failed') })
-
-# test_that('bad paths fail', { expect_error(set_tinystan_path('badpath'),
-# 'does not exist!') expect_error(set_tinystan_path(file.path(stan_folder)),
-# 'does not contain file 'Makefile'') })
+test_that("bad paths fail", {
+    expect_error(set_tinystan_path("badpath"), "does not exist!")
+    expect_error(set_tinystan_path(file.path(stan_folder)), "does not contain file 'Makefile'")
+})
