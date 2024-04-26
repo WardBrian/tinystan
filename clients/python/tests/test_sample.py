3ed961f24fe8bca81d468403b4af50866c29290a
Brian Ward <bward@flatironinstitute.org>
2024-04-26 12:33:04 -0400

    Fix sundials test path

--- clients/python/tests/test_sample.py
+++ clients/python/tests/test_sample.py
@@ -103,7 +103,7 @@ def test_save_metric(gaussian_model):
 def test_sundials_ode():
     sir = tinystan.Model(STAN_FOLDER / "sir" / "sir_model.so")
     sir_data = STAN_FOLDER / "sir" / "sir.data.json"
-    sir_init = STAN_FOLDER / "sir" / "sir.data.json"
+    sir_init = STAN_FOLDER / "sir" / "sir.init.json"
 
     out = sir.sample(
         data=sir_data,
