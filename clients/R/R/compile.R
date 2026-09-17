bb39f47428570c0a7f3753dedf4c189ec3ebea0f
Brian Ward <bward@flatironinstitute.org>
2026-09-17 10:48:18 -0400

    fixes

--- clients/R/R/compile.R
+++ clients/R/R/compile.R
@@ -105,7 +105,7 @@ compile_model <- function(stan_file, stanc_args = NULL, make_args = NULL) {
   stancflags <- paste("--include-paths=.", paste(stanc_args, collapse = " "))
 
   flags <- c(
-    paste("-C", shQuote(get_tinystan_path()),
+    paste("-C", shQuote(get_tinystan_path())),
     make_args,
     paste0("STANCFLAGS=", shQuote(stancflags)),
     shQuote(output)
