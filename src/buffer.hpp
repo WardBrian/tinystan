bcc6714ad7bf34f9a4344cc6d8513d62a1f98566
Brian Ward <bward@flatironinstitute.org>
2026-09-17 11:05:45 -0400

    attempt to fix macos

--- src/buffer.hpp
+++ src/buffer.hpp
@@ -41,8 +41,8 @@ namespace io {
  */
 class buffer_writer : public stan::callbacks::writer {
  public:
-  buffer_writer(double *buf, size_t max) : buf(buf), pos(0), size(max){};
-  virtual ~buffer_writer(){};
+  buffer_writer(double *buf, size_t max) : buf(buf), pos(0), size(max) {};
+  virtual ~buffer_writer() {};
 
   /**
    * Primary method used by the Stan algorithms
@@ -117,7 +117,7 @@ class buffer_writer : public stan::callbacks::writer {
 class filtered_writer : public stan::callbacks::structured_writer {
  public:
   filtered_writer() : filters{} {};
-  virtual ~filtered_writer(){};
+  virtual ~filtered_writer() {};
 
   void add_key(const std::string &key_in, double *buf) {
     if (buf != nullptr) {
@@ -155,6 +155,10 @@ class filtered_writer : public stan::callbacks::structured_writer {
     }
   }
 
+  // workaround for
+  // https://github.com/stan-dev/stan/pull/3406#issuecomment-5716227487
+  void write(const std::string &, std::size_t) override {}
+
   // other write methods are currently unused by anything we need
   using stan::callbacks::structured_writer::write;
 
@@ -172,8 +176,8 @@ class inv_metric_buffer_reader : public stan::io::empty_var_context {
  public:
   inv_metric_buffer_reader(const double *buf, size_t size,
                            TinyStanMetric metric_choice)
-      : buf(buf), size(size), dense(metric_choice == TinyStanMetric::dense){};
-  virtual ~inv_metric_buffer_reader(){};
+      : buf(buf), size(size), dense(metric_choice == TinyStanMetric::dense) {};
+  virtual ~inv_metric_buffer_reader() {};
 
   bool contains_r(const std::string &name) const override {
     return name == "inv_metric";
